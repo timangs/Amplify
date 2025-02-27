9867b5131a30f0cc1504bcd4467e9cd4f62f50e5
timang <jeongkyu1145@gmail.com>
2025-02-27 12:14:18 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_0OQksyMHA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7oitb91ue1vlra6tmmdlrmr0oq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_qQ37bu5ou', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1ki81avbmbki7e52e76kvea650', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
